664f61e74b62128d70bc13f85cbedbd65d04d167
pro100cuber@gmail.com <pro100cuber@gmail.com>
2024-03-16 19:23:55 +0700

    update main

--- main.php
+++ main.php
@@ -1,14 +1,29 @@
 <?php
+$coroResult = [];
+go(function () use (&$coroResult) {
 
-
-$var = parallel\run(function () {
-    $coroResult = [];
-    go(function () use (&$coroResult) {
-        sleep(1);
-        $coroResult[] = 1;
+    $future = parallel\run(function () {
+        $result = 0;
+        foreach (range(0, 10000000) as $key => $item) {
+            $result += $item;
+            if ($key % 1000 === 0) {
+                $result /= 1000;
+            }
+        }
+        return $result;
     });
+    go(function () use ($future, &$coroResult) {
+        while (!$future->done()) {
+            usleep(1000);
+        }
+        $coroResult[0] = $future->value();
+    });
+    go(function () use (&$coroResult) {
+        foreach (range(0, 500) as $item) {
+            $coroResult[] = $item;
+            usleep(1000);
+        }
+    });
+});
 
-    return $coroResult;
-})->value();
-
-var_dump($var);
+var_dump($coroResult);
