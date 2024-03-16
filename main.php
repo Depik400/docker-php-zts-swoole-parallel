4053cea9999851ddab823590066b355559d01be2
pro100cuber@gmail.com <pro100cuber@gmail.com>
2024-03-16 19:36:12 +0700

    fix: example. stupid async

--- main.php
+++ main.php
@@ -1,29 +1,29 @@
 <?php
+\Swoole\Runtime::enableCoroutine();
 $coroResult = [];
 go(function () use (&$coroResult) {
-
+    $channel = new chan();
     $future = parallel\run(function () {
         $result = 0;
         foreach (range(0, 10000000) as $key => $item) {
-            $result += $item;
-            if ($key % 1000 === 0) {
-                $result /= 1000;
-            }
+            $result += sin($item);
         }
         return $result;
     });
     go(function () use ($future, &$coroResult) {
         while (!$future->done()) {
-            usleep(1000);
+            usleep(700);
         }
         $coroResult[0] = $future->value();
     });
-    go(function () use (&$coroResult) {
+    go(function () use (&$coroResult, $channel) {
         foreach (range(0, 500) as $item) {
             $coroResult[] = $item;
             usleep(1000);
         }
+        $channel->push($coroResult);
     });
-});
+    $item = $channel->pop(-1);
 
-var_dump($coroResult);
+    var_dump($item);
+});
